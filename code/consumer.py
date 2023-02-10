a6c5ed7b5381aea751becbd01c4ab5a76398a557
Trung Kiên <ninhtrungkien2811@gmail.com>
2023-02-10 16:32:38 +0700

    Update code

--- code/consumer.py
+++ code/consumer.py
@@ -1,9 +1,32 @@
 from kafka import KafkaConsumer
 import pydoop.hdfs as hdfs
-consumer = KafkaConsumer('TutorialTopic',bootstrap_servers=['localhost:9092'])
-hdfs_path = 'hdfs://localhost:9000/GasData/natural-gas.txt'
- 
+import time
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+import json
+# Create spark session
+sc = SparkContext('local')
+spark = SparkSession(sc)
+sqlContext = SQLContext(sc)
+
+consumer = KafkaConsumer('GasData',bootstrap_servers=['10.157.60.165:9092'])
+
+schema = StructType([
+    StructField("change", StringType(), True),
+    StructField("date", StringType(), True),
+    StructField("fuelType", StringType(), True),
+    StructField("percentChange", StringType(), True),
+    StructField("x", StringType(), True),
+    StructField("y", StringType(), True)
+    # Add more fields as required
+])
+
 for message in consumer:
-   with hdfs.open(hdfs_path, 'at') as f:
-      print(message.value)
-      f.write(message.value.decode('utf-8'))
+    msg = message.value.decode("utf-8") 
+    tmp = json.loads(msg)
+    print(tmp)
+    df = spark.createDataFrame(tmp, schema)
+    df.show()
+    df.write.format('csv') .mode('append').option("header", "true").save("hdfs://10.157.60.165:9000/GasData/dataCsv")
--- code/producer.py
+++ code/producer.py
@@ -1,21 +1,48 @@
-from kafka import KafkaProducer
-from json import dumps
+import requests
 import json
+import time
+import sys
+from kafka import KafkaProducer
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda K:dumps(K).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=['10.157.60.165:9092'],value_serializer=lambda K:json.dumps(K).encode('utf-8'))
+fuelType = None
 
-# Open the JSON file
-with open('./crawl-gas/natural-gas.json', 'r') as f:
-    # Read the file line by line
-    for line in f:
-        # Parse the JSON data
-        data = json.loads(line)
-        # Send the data to the Kafka topic
-        producer.send('TutorialTopic', value=data)
+def addition(n):
+    return {
+        "fuelType": sys.argv[1],
+        **n
+    }
 
-# Flush the producer to ensure all messages are sent
-producer.flush()
+def main():
+    if len(sys.argv) < 2:
+        print('Missing argument')
+        return
+        
+    fuelType = sys.argv[1]
+    authToken = input("Enter auth token: ")
+    yearStart = 2021
+    yearEnd = 2023
 
-# for e in range(100):
-#     data = {'number' : e}
-#     producer.send('TutorialTopic', value=data)
+    res = []
+    for y in range(yearStart, yearEnd):
+        response = requests.get(
+            f"https://markets.tradingeconomics.com/chart?s=ng1:com&d1={y}-01-01&d2={y+1}-01-01&interval=1d&securify=new&url=/commodity/{fuelType}&AUTH={authToken}&ohlc=0"
+        )
+        if response.json() != None:
+            print('fuelType: %s', fuelType)
+            data = response.json().get("series", [{}])[0].get("data", [])
+            # res.extend(data)    
+            print('Length:', len(data) or 0)
+            tmp = map(addition, data)
+            res.extend(list(tmp))
+            sendValue = json.loads(json.dumps(res))
+            producer.send('GasData', value=sendValue)
+            time.sleep(60)
+            res = []
+        else:
+            print('No Data')
+            time.sleep(30)
+            continue
+
+
+main()
